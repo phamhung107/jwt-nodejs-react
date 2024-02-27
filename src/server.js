8f1396190a4278fd85b8fd6493d97c6f7c3c9887
phamhung107 <pvh1072002@gmail.com>
2024-02-27 21:50:53 +0700

    create new users

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = process.PORT || 8080;
 //config view engine
 configViewEngine(app);
+//config body-parser
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 //init web routes
 initWebRoutes(app);
 
