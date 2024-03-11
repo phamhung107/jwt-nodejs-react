be1337c5ab99c4a627e22b1060ccee2d70d03f6f
hungpham107 <118520911+hungpham107@users.noreply.github.com>
2024-03-11 07:53:47 +0700

    Axios Post Data

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,4 +1,9 @@
 const testApi = (req, res) => {
   return res.status(200).send("Test API success");
 };
-module.exports = { testApi };
+
+const handleRegister = (req, res) => {
+  console.log("ok", req.body);
+};
+
+module.exports = { testApi, handleRegister };
